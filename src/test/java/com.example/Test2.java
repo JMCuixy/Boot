6e6fe7c1cbf0b6121e4e021b1c1c2f3b0c1b70c2
cuixiuyin <1099442418@qq.com>
2018-09-25 12:46:45 +0800

    #SpringBoot # SpringBoot + Redis 集成

---
+++ src/test/java/com.example/Test2.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Example.class)
+public class Test2 {
+
+    private static final Logger logger = LoggerFactory.getLogger(Test2.class);
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    @Test
+    public void test2() {
+        redisTemplate.opsForValue().set("springBoot", "SpringDateRedis");
+        logger.info("Redis 数据库中获取到的值是：{}", redisTemplate.opsForValue().get("springBoot"));
+    }
+
+}
