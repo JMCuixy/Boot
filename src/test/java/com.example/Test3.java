16da1eca2583211c799f2733a1e5eee51c680ed2
cuixiuyin <1099442418@qq.com>
2018-09-25 12:54:21 +0800

    #SpringBoot # SpringBoot + MongoDB 集成

---
+++ src/test/java/com.example/Test3.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import net.bytebuddy.asm.Advice;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Example.class)
+public class Test3 {
+
+    @Autowired
+    private MongoOperations mongoOperations;
+
+}
