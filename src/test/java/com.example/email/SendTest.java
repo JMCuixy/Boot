8fe95358b326cc2dabc3f6e62b0cbf2ea181172b
cuixiuyin <1099442418@qq.com>
2018-10-08 13:05:50 +0800

    #SpringBoot #简单集成邮件服务——添加附件

--- src/test/java/com.example/email/SendTest.java
+++ src/test/java/com.example/email/SendTest.java
@@ -5,9 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Example.class)
@@ -27,6 +32,24 @@ public class SendTest {
         message.setSubject("邮件主题");
         message.setText("邮件内容");
         javaMailSender.send(message);
+    }
+
+    /**
+     * 附件发送
+     */
+    @Test
+    public void mimeEmail() throws MessagingException {
+        // MimeMessage 本身的 API 有些笨重，我们可以使用 MimeMessageHelper
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        // 第二个参数是 true ，表明这个消息是 multipart类型的/
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setFrom("15980292662@163.com");
+        mimeMessageHelper.setTo("cuixiuyin@qianxx.com");
+        mimeMessageHelper.setSubject("附件邮件主题");
+        mimeMessageHelper.setText("附件邮件内容");
+        //添加附件,第一个参数表示添加到 Email 中附件的名称，第二个参数是图片资源
+        mimeMessageHelper.addAttachment("boot.png", new ClassPathResource("public/images/boot.png"));
+        javaMailSender.send(mimeMessage);
     }
 
 }
