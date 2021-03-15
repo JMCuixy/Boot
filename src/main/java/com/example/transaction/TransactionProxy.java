e4d2737bc054a454f33c1d91f3fb43918c5ac38b
xiuyin.cui <xiuyin.cui@joymo.tech>
2021-03-15 11:27:01 +0800

    事务控制

--- src/main/java/com/example/transaction/TransactionProxy.java
+++ src/main/java/com/example/transaction/TransactionProxy.java
@@ -1,25 +1,49 @@
 package com.example.transaction;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * @Author: xiuyin.cui@joymo.tech
  * @Date: 2021/3/13 17:43
  * @Description: 基于 AOP 的事务操作
  */
+@Component
 public class TransactionProxy {
-    //proxy为代理的接口
-    public static final Logger LOGGER = LoggerFactory.getLogger(TransactionProxy.class);
 
-    private static final ThreadLocal<Boolean> FLAG_HOLDER = new
-            ThreadLocal<Boolean>() {
-                @Override
-                protected Boolean initialValue() {
-                    return false;
-                }
-            };
+    @Autowired
+    private DataSourceTransactionManager dataSourceTransactionManager;
 
-
+    /**
+     * 通过 PlatformTransactionManager 使用事务
+     */
+    public void test1() {
+        DefaultTransactionDefinition defaultTransactionDefinition = new DefaultTransactionDefinition();
+        TransactionStatus transaction = dataSourceTransactionManager.getTransaction(defaultTransactionDefinition);
+        try {
+            // do something
+        } catch (Exception e) {
+            dataSourceTransactionManager.rollback(transaction);
+        }
+        dataSourceTransactionManager.commit(transaction);
     }
+
+
+    /**
+     * 通过 TransactionTemplate 使用事务
+     */
+    public void test2() {
+        TransactionTemplate transactionTemplate = new TransactionTemplate(dataSourceTransactionManager);
+        Object execute = transactionTemplate.execute(status -> {
+            // do something
+            return new Object();
+        });
+    }
+
+
+}
